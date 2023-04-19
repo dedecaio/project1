8e5427784fc1b142dbd51700e30dc4b7539db344
Caio <caio.florees2005@gmail.com>
2023-04-19 19:45:31 -0300

    Alterações

--- tasks/views.py
+++ tasks/views.py
@@ -1,5 +1,6 @@
 from django.shortcuts import render
-from django.http import HttpResponse
+from django.http import HttpResponse  # noqa
+
 
 def tasks(request, *args, **kwargs):
-    return HttpResponse("Olá Mundo")
+    return render(request, 'tasks/index.html',)
